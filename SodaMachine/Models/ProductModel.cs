d8a4561aac74c5422058797b83e5178a9349897c
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-23 19:03:22 -0600

    Create new model for the product

---
+++ SodaMachine/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace SodaMachine.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; } = 0;
+
+        public string ProductName { get; set; } = string.Empty;
+
+        public double Price { get; set; } = 0.0;
+
+        public string? ProductImage { get; set; }
+
+    }
+}
