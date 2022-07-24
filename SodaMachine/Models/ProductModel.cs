d8b972836a17711838b292eaabba3567cbe4dece
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-23 21:19:14 -0600

    Add a new model for product

--- SodaMachine/Models/ProductModel.cs
+++ SodaMachine/Models/ProductModel.cs
@@ -10,6 +10,8 @@ namespace SodaMachine.Models
 
         public double Price { get; set; } = 0.0;
 
+        public int Quantity { get; set; } = 0;
+
         public string? ProductImage { get; set; }
 
     }
