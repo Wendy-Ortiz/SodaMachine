66fa4818fe2a6ce642dfd9ef9f01249366fa9e74
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-23 19:04:02 -0600

    Modify controller for new soda machine functionality

--- SodaMachine/Controllers/HomeController.cs
+++ SodaMachine/Controllers/HomeController.cs
@@ -15,7 +15,9 @@ namespace SodaMachine.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var products = GetListOfProducts();
+            ViewBag.MainTitle = "MÁQUINA EXPENDEDORA DE REFRESCOS";
+            return View(products);
         }
 
         public IActionResult Privacy()
@@ -27,6 +29,16 @@ namespace SodaMachine.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public List<ProductModel> GetListOfProducts()
+        {
+            List<ProductModel> products = new List<ProductModel>();
+            products.Add(new ProductModel { ProductId = 1, ProductName = "Coca Cola", Price = 500, ProductImage = "images/coca.jpg" });
+            products.Add(new ProductModel { ProductId = 2, ProductName = "Pepsi", Price = 600, ProductImage = "images/pepsi.jpg" });
+            products.Add(new ProductModel { ProductId = 3, ProductName = "Fanta", Price = 550, ProductImage = "images/fanta.jpg" });
+            products.Add(new ProductModel { ProductId = 1, ProductName = "Sprite", Price = 725, ProductImage = "images/sprite.jpg" });
+            return products;
         }
     }
 }
