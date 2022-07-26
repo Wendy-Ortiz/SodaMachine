a4c4e95f2f6d6ad6bb77b0eed83627ef399e2b10
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-26 11:44:27 -0600

    Added allowed cash

--- SodaMachine/Handlers/CashHandler.cs
+++ SodaMachine/Handlers/CashHandler.cs
@@ -5,6 +5,7 @@ namespace SodaMachine.Handlers
     public class CashHandler
     {
         List<CashModel> availableCash = new List<CashModel>();
+        List<CashModel> cashAllowed = new List<CashModel>();
 
         public CashHandler()
         {
@@ -18,6 +19,16 @@ namespace SodaMachine.Handlers
             availableCash.Add(new CashModel { Type = "coin", Value = 50, Quantity = 50 });
             availableCash.Add(new CashModel { Type = "coin", Value = 25, Quantity = 25 });
             return availableCash;
+        }
+
+        public List<CashModel> GetAllowedCash()
+        {
+            cashAllowed.Add(new CashModel { Type = "coin", Value = 500, Quantity = 0 });
+            cashAllowed.Add(new CashModel { Type = "coin", Value = 100, Quantity = 0 });
+            cashAllowed.Add(new CashModel { Type = "coin", Value = 50, Quantity = 0 });
+            cashAllowed.Add(new CashModel { Type = "coin", Value = 25, Quantity = 0 });
+            cashAllowed.Add(new CashModel { Type = "bill", Value = 1000, Quantity = 0 });
+            return cashAllowed;
         }
     }
 }
