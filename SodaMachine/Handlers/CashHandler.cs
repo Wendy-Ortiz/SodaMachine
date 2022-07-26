86fff09a4402c6ec92d9b5be99a1d3392ca4b837
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-26 11:34:02 -0600

    Added data for available cash for change

--- SodaMachine/Handlers/CashHandler.cs
+++ SodaMachine/Handlers/CashHandler.cs
@@ -4,12 +4,20 @@ namespace SodaMachine.Handlers
 {
     public class CashHandler
     {
-        List<CashModel> cash = new List<CashModel>();
+        List<CashModel> availableCash = new List<CashModel>();
 
         public CashHandler()
         {
             var builder = WebApplication.CreateBuilder();
         }
-
+        
+        public List<CashModel> GetAvailableCash()
+        {
+            availableCash.Add(new CashModel { Type = "coin", Value = 500, Quantity = 20});
+            availableCash.Add(new CashModel { Type = "coin", Value = 100, Quantity = 30 });
+            availableCash.Add(new CashModel { Type = "coin", Value = 50, Quantity = 50 });
+            availableCash.Add(new CashModel { Type = "coin", Value = 25, Quantity = 25 });
+            return availableCash;
+        }
     }
 }
